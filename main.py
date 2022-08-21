6a9b70cea8d96ab0a4f8f4ef979c72f613b65ee8
loudeb <86251202+loudeb@users.noreply.github.com>
2022-08-21 16:47:47 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("My SQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}')")
+    return connection
+connection = create_server_connection("localhost", "root", "Louis123")
